881a1f0a9c61d84c810472a97e101b14b7d404aa
John Li <johnzli1995@gmail.com>
2025-04-11 00:59:11 -0700

    hmmm

--- server/src/models/prayer_requests_storage.ts
+++ server/src/models/prayer_requests_storage.ts
@@ -1,5 +1,6 @@
 import { getPool } from '../db';
-import { ListPrayerRequestsParams, PrayerRequest } from './storage_types';
+import { PrayerRequest } from '../storage_types';
+import { ListPrayerRequestsParams } from './storage_types';
 
 const SqlCommands = {
   ListPrayerRequests: `
