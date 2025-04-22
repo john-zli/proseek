051ed37ddf018a3c2290a9f4400c9f2b9c727c3f
John Li <johnzli1995@gmail.com>
2025-04-22 01:47:51 -0700

    Really good progres

--- client/src/components/modals/contact_info_modal.tsx
+++ client/src/components/modals/contact_info_modal.tsx
@@ -3,6 +3,7 @@ import { useCallback, useContext, useState } from 'react';
 
 import { ModalContext } from '../../contexts/modal_context_provider';
 import { Button, ButtonStyle } from '../../shared-components/button';
+import { CheckboxView } from '../../shared-components/checkbox_view';
 import { ModalContainer } from '../../shared-components/modal_container';
 import classes from './contact_info_modal.module.less';
 
@@ -14,18 +15,6 @@ interface ContactMethods {
 interface Props {
   onSubmit: (email: string, phone: string) => void;
 }
-
-const CheckIcon = () => (
-  <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M11.6666 3.5L5.24992 9.91667L2.33325 7"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
 
 const formatPhoneNumber = (value: string): string => {
   // Remove all non-numeric characters
@@ -76,16 +65,16 @@ export function ContactInfoModal({ onSubmit }: Props) {
   );
 
   const onEmailChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    e.stopPropagation();
-    e.preventDefault();
     setEmail(e.target.value);
   }, []);
 
   const onPhoneChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    e.stopPropagation();
-    e.preventDefault();
     const formattedPhone = formatPhoneNumber(e.target.value);
     setPhone(formattedPhone);
+  }, []);
+
+  const stopPropagation = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
   }, []);
 
   const handleSubmit = useCallback(() => {
@@ -107,7 +96,7 @@ export function ContactInfoModal({ onSubmit }: Props) {
     <ModalContainer>
       <div className={classes.container}>
         <h2>How can we reach you?</h2>
-        <p>Please select how you would like to be notified when a church responds to your prayer request.</p>
+        <p>(Optional): Select how you would like to be notified when a church responds to your prayer request.</p>
 
         <div className={classes.form}>
           <div className={classes.checkboxGroup}>
@@ -115,15 +104,10 @@ export function ContactInfoModal({ onSubmit }: Props) {
               className={clsx(classes.customCheckbox, { [classes.checked]: contactMethods.email })}
               onClick={() => handleContactMethodChange('email')}
             >
-              <div className={classes.checkboxContent}>
-                <div className={clsx(classes.checkbox, { [classes.checked]: contactMethods.email })}>
-                  {contactMethods.email && <CheckIcon />}
-                </div>
-                <span className={classes.checkboxLabel}>Email</span>
-              </div>
+              <CheckboxView checked={contactMethods.email} label="Email" />
               {contactMethods.email && (
                 // We don't want to uncheck the checkbox when the input is focused
-                <div className={classes.inputGroup} onClick={e => e.stopPropagation()}>
+                <div className={classes.inputGroup} onClick={stopPropagation}>
                   <input
                     type="email"
                     id="email"
@@ -140,22 +124,17 @@ export function ContactInfoModal({ onSubmit }: Props) {
               className={clsx(classes.customCheckbox, { [classes.checked]: contactMethods.text })}
               onClick={() => handleContactMethodChange('text')}
             >
-              <div className={classes.checkboxContent}>
-                <div className={clsx(classes.checkbox, { [classes.checked]: contactMethods.text })}>
-                  {contactMethods.text && <CheckIcon />}
-                </div>
-                <span className={classes.checkboxLabel}>Text Message</span>
-              </div>
+              <CheckboxView checked={contactMethods.text} label="Text Message" />
               {contactMethods.text && (
                 // We don't want to uncheck the checkbox when the input is focused
-                <div className={classes.inputGroup} onClick={e => e.stopPropagation()}>
+                <div className={classes.inputGroup} onClick={stopPropagation}>
                   <input
                     type="tel"
                     id="phone"
                     value={phone}
                     onChange={onPhoneChange}
                     placeholder="(123) 456-7890"
-                    className={`${classes.input} ${phoneError ? classes.error : ''}`}
+                    className={clsx(classes.input, { [classes.error]: phoneError })}
                   />
                   {phoneError && <span className={classes.errorText}>{phoneError}</span>}
                 </div>
@@ -171,11 +150,7 @@ export function ContactInfoModal({ onSubmit }: Props) {
           <Button
             buttonStyle={ButtonStyle.Primary}
             onClick={handleSubmit}
-            disabled={
-              (contactMethods.email && !email) ||
-              (contactMethods.text && !isValidPhoneNumber(phone)) ||
-              (!contactMethods.email && !contactMethods.text)
-            }
+            disabled={(contactMethods.email && !email) || (contactMethods.text && !isValidPhoneNumber(phone))}
           >
             Submit
           </Button>
